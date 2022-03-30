f0f0ca64e356cfba5fc9f1b0ea234664a33409c5
CuteKittyMeowMeow <102677118+CuteKittyMeowMeow@users.noreply.github.com>
2022-03-31 02:01:45 +0300

    Create Program.cs

---
+++ SortedAray/Program.cs
@@ -0,0 +1,30 @@
+class Pogram
+{
+
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Введите массив через пробел");
+        int[] array = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+        Console.WriteLine("Введенный массив:");
+
+        foreach (int i in array)
+            Console.Write(i+", ");
+        Console.WriteLine();
+
+        int incCount = 0;
+        int decCount = 0;
+                      
+        for (int i = 0; i < array.Length - 1; i++) 
+        {
+            if (array[i] < array[i + 1]) incCount++;
+            else if (array[i] > array[i + 1]) decCount++;
+        }
+
+        if (incCount != 0 && decCount != 0)
+            Console.WriteLine("Неупорядоченный массив");
+        else if (incCount == 0 && decCount ==0) Console.WriteLine("Массив из одинаковых чисел");
+        else if (incCount == 0) Console.WriteLine("Массив по убыванию");
+        else Console.WriteLine("Массив по возрастанию");
+
+    }
+}
